8ec968f34ee73598b21f91bca46d98266da83dab
joes574 <js260292@my.stchas.edu>
2024-04-08 01:21:04 -0500

    Updated test case files

--- Team-Project-2/test_case_1.cpp
+++ Team-Project-2/test_case_1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <fstream>
+#include <string>
 /*
 Test case 1 analysis with file "Infix_Expression_tc1"
 */
@@ -7,5 +8,12 @@ Test case 1 analysis with file "Infix_Expression_tc1"
 using namespace std;
 
 void test_case_1() {
+	ifstream input_file;
+	string infix_expression = "";
 
+	input_file.open("Infix_Expression_tc1.txt");
+
+	getline(input_file, infix_expression);
+
+	input_file.close();
 }
--- Team-Project-2/test_case_2.cpp
+++ Team-Project-2/test_case_2.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 /*
 Test case 2 analysis with file "Infix_Expression_tc2"
@@ -7,5 +9,12 @@ Test case 2 analysis with file "Infix_Expression_tc2"
 using namespace std;
 
 void test_case_2() {
+	ifstream input_file;
+	string infix_expression = "";
 
+	input_file.open("Infix_Expression_tc2.txt");
+
+	getline(input_file, infix_expression);
+
+	input_file.close();
 }
